8b9cbe917e74a7f957494b9b858cba358c84eb94
craigbrown313 <craigbrown313@gmail.com>
2020-03-26 15:22:55 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-    $con = new PDO("mysql:dbname=B3C;host=mysql://b23042a38900ed:da3b2005@us-cdbr-iron-east-01.cleardb.net/heroku_81eef3d874c129c?reconnect=truelocalhost", "root", "");
+    $con = new PDO("mysql:dbname=B3C;host=b23042a38900edt", "da3b2005", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
